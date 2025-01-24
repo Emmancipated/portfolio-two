bb1f4ae5aac24be43a2edb3ef57acb6650e32046
Emmanuel <emmancipationera@gmail.com>
2025-01-25 00:57:16 +0100

    fix: add .env file

---
+++ app/.well-known/assetlinks.json/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const assetlinks = [
+    {
+      relation: ["delegate_permission/common.handle_all_urls"],
+      target: {
+        namespace: "android_app",
+        package_name: "com.emmanuel_ck.handiehubexpo",
+        sha256_cert_fingerprints: [process.env.FINGERPRINT_SHA256],
+      },
+    },
+  ];
+
+  return NextResponse.json(assetlinks, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "public, max-age=0, must-revalidate",
+    },
+  });
+}
